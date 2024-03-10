03ffe71df5db2626f9f85d260e96f4b630bd9308
Cristian Velasquez Ramos <seeve.c@gmail.com>
2024-03-10 14:47:45 -0400

    make focus ring always accented

--- apps/web/app/ui/button.tsx
+++ apps/web/app/ui/button.tsx
@@ -3,14 +3,14 @@ import * as React from 'react';
 import { ButtonProps, Button as RACButton } from 'react-aria-components';
 
 const base =
-  'text-base uppercase py-5 px-10 font-bold inline-flex items-center justify-center gap-2 border-2 border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2 duration-200 rounded-sm';
+  'text-base uppercase py-5 px-10 font-bold inline-flex items-center justify-center gap-2 border-2 border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2 duration-200 rounded-sm focus:ring-accent-500';
 const variants = {
   accent:
-    'bg-accent-600 text-accent-50 hover:bg-accent-700 focus:ring-accent-500 data-[pressed]:bg-accent-500',
+    'bg-accent-600 text-accent-50 hover:bg-accent-700 data-[pressed]:bg-accent-500',
   positive:
-    'bg-neutral-50 text-neutral-950 hover:bg-neutral-200 focus:ring-neutral-900 data-[pressed]:bg-neutral-300',
+    'bg-neutral-50 text-neutral-950 hover:bg-neutral-200 data-[pressed]:bg-neutral-300',
   negative:
-    'bg-neutral-900 text-neutral-50 hover:bg-neutral-800 focus:ring-neutral-50 data-[pressed]:bg-neutral-700',
+    'bg-neutral-900 text-neutral-50 hover:bg-neutral-800 data-[pressed]:bg-neutral-700',
 } as const;
 
 export type ButtonVariant = keyof typeof variants;
