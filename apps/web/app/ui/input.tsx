9601a16653f58a5af42e353c4f48556a9eca4df7
Cristian Velasquez Ramos <seeve.c@gmail.com>
2024-03-31 18:23:42 -0400

    fix input

--- apps/web/app/ui/input.tsx
+++ apps/web/app/ui/input.tsx
@@ -2,8 +2,6 @@ import clsx from 'clsx';
 import * as React from 'react';
 import { InputProps, Input as RACInput } from 'react-aria-components';
 
-const base =
-  'text-base py-5 px-6 rounded-sm border-2 border-transparent focus:outline-none focus:border-b-accent-600 duration-200 placeholder-neutral-400 group-data-[variant="positive"]:text-neutral-950 group-data-[variant="negative"]:text-neutral-50 group-data-[variant="positive"]:bg-neutral-50 group-data-[variant="negative"]:bg-neutral-900';
 const variants = {
   positive: 'bg-neutral-50 text-neutral-950',
   negative: 'bg-neutral-900 text-neutral-50',
@@ -21,7 +19,11 @@ const Input = React.forwardRef<
     <RACInput
       {...props}
       ref={ref}
-      className={clsx(base, variants[variant], className)}
+      className={clsx(
+        'focus:border-b-accent-600 rounded-sm border-b-2 px-6 py-5 text-base placeholder-neutral-400 outline-none duration-200 group-data-[variant="negative"]:bg-neutral-900 group-data-[variant="positive"]:bg-neutral-50 group-data-[variant="negative"]:text-neutral-50 group-data-[variant="positive"]:text-neutral-950',
+        variants[variant],
+        className,
+      )}
     />
   );
 });
